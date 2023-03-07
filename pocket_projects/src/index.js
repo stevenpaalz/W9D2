c67c1aed32293122775d0d33defb566998dc1aab
abaez8031 <abaez8031@gmail.com>
2023-03-07 16:28:36 -0500

    finished clock

--- pocket_projects/src/index.js
+++ pocket_projects/src/index.js
@@ -1,7 +1,8 @@
 import warmUp from "./warmup";
-import clock from "./clock";
+import Clock from "./clock";
 import dropDown from "./drop_down";
 import toDoList from "./todo_list";
 import slideScroll from "./slide_scroll";
 
-
+const clockElement = document.getElementById("clock");
+const c = new Clock();
--- pocket_projects/src/warmup.js
+++ pocket_projects/src/warmup.js
@@ -1,10 +1,14 @@
-
-const partyHeader = document.getElementById('party');
+const partyHeader = document.getElementById("party");
 
 export const htmlGenerator = (string, htmlElement) => {
-    const p = document.createElement("p")
-    p.innerText = string;
+  const p = document.createElement("p");
+  p.innerText = string;
+  if (htmlElement.children.length > 0) {
+    htmlElement.children[0].remove();
     htmlElement.append(p);
+  } else {
+    htmlElement.append(p);
+  }
 };
 
-htmlGenerator('Party Time.', partyHeader);
+htmlGenerator("Party Time.", partyHeader);

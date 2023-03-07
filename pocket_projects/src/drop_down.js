816ed5c26de95539e502d36778906929a240b53a
Steven Paalz <stevenpaalz@gmail.com>
2023-03-07 17:07:13 -0500

    beautiful styles

--- pocket_projects/src/drop_down.js
+++ pocket_projects/src/drop_down.js
@@ -9,3 +9,43 @@ const dogs = {
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
 
+function dogLinkCreator() {
+  const dogsArray = [];
+  for (let key in dogs) {
+    let a = document.createElement("a");
+    a.innerHTML = key;
+    a.href = dogs[key];
+    let li = document.createElement("li");
+    li.classList.add("dog-link");
+    li.append(a);
+    dogsArray.push(li);
+  }
+  return dogsArray;
+}
+
+const dropDownDogList = document.querySelector(".drop-down-dog-list")
+
+function attachDogLinks() {
+  const dogLinks = dogLinkCreator();
+  dogLinks.forEach((link) => {
+    dropDownDogList.append(link);
+  })
+}
+
+attachDogLinks();
+
+const dropDownDogNav = document.querySelector(".drop-down-dog-nav");
+
+dropDownDogNav.addEventListener('mouseover', handleEnter)
+dropDownDogNav.addEventListener('mouseout', handleLeave)
+
+
+function handleEnter() {
+  dropDownDogList.classList.toggle("hidden")
+}
+
+function handleLeave() {
+  dropDownDogList.classList.toggle("hidden")
+}
+
+export * from "./drop_down"
--- pocket_projects/src/index.js
+++ pocket_projects/src/index.js
@@ -1,6 +1,6 @@
 import warmUp from "./warmup";
 import Clock from "./clock";
-import dropDown from "./drop_down";
+import "./drop_down";
 import toDoList from "./todo_list";
 import slideScroll from "./slide_scroll";
 

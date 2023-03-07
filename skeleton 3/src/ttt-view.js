bea775fcfd6944a96a8208b429e3f1a73e21d0b4
abaez8031 <abaez8031@gmail.com>
2023-03-07 15:33:50 -0500

    finished ttt

--- skeleton 3/src/ttt-view.js
+++ skeleton 3/src/ttt-view.js
@@ -3,6 +3,7 @@ class View {
     this.game = game;
     this.el = el;
     this.setupBoard();
+    this.bindEvents();
   }
 
   setupBoard() {
@@ -10,12 +11,13 @@ class View {
 
     for (let i = 0; i < this.game.board.grid.length; i++) {
       let row = this.game.board.grid[i];
-      for (let j =0; j < row.length; j++) {
+      for (let j = 0; j < row.length; j++) {
         let cell = row[j];
         let li = document.createElement("li");
         li.dataset.row = i;
         li.dataset.col = j;
-        li.classList.add("cells")
+        li.innerText = cell;
+        li.classList.add("cells");
         ul.append(li);
       }
     }
@@ -23,26 +25,38 @@ class View {
     ul.style.display = "flex";
     ul.style.width = "600px";
     ul.style.flexWrap = "wrap";
-    
-    ttt.append(ul);
 
+    ttt.append(ul);
   }
 
-  bindEvents() {}
+  bindEvents() {
+    const ul = document.querySelector("ul");
+    ul.addEventListener("click", this.handleClick.bind(this));
+  }
 
   handleClick(e) {
     let row = e.target.dataset.row;
     let col = e.target.dataset.col;
     let square = [row, col];
-    this.makeMove(square);
+    let that = e.target;
+    this.makeMove(square, that);
   }
 
-  makeMove(square) {
-    if (!this.game.board.isEmptyPos(square)) {alert('Is not an empty position!')}
-    else {
-      this.game.playMove(square)
-      e.target.classList.remove(".cells");
-      e.target.classList.add(".clicked");
+  makeMove(square, that) {
+    if (!this.game.board.isEmptyPos(square)) {
+      alert("Is not an empty position!");
+    } else {
+      this.game.playMove(square);
+      that.innerText = this.game.board.grid[square[0]][square[1]];
+      that.classList.add("clicked");
+      if (that.innerText === "x") {
+        that.classList.add("red");
+      } else {
+        that.classList.add("blue");
+      }
+      if (this.game.isOver()) {
+        alert(`Congrats ${this.game.winner()}`);
+      }
     }
   }
 }
